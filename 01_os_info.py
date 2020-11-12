24c3935e992557681f292e45b2a4899724393d50
Igor <vlasov.work@gmail.com>
2020-11-12 22:10:19 +0300

    Получает инфо о версии

--- 01_os_info.py
+++ 01_os_info.py
@@ -2,4 +2,8 @@
 import platform
 import sys
 
-print("222")
+info = 'OS info is \n{} \n\nPython version is {} {}'.format(platform.uname(), sys.version, platform.architecture())
+print(info)
+
+with open('os_info.txt', 'w') as ff:
+    ff.write(info)
