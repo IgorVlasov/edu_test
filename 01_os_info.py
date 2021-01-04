fa389d0335030e8b80463341247b7e16b1bb042b
Igor <vlasov.work@gmail.com>
2021-01-04 12:08:01 +0300

    Test

--- 01_os_info.py
+++ 01_os_info.py
@@ -4,6 +4,7 @@ import sys
 
 info = 'OS info is \n{} \n\nPython version is {} {}'.format(platform.uname(), sys.version, platform.architecture())
 print(info)
+print(info)
 
 with open('os_info.txt', 'w') as ff:
     ff.write(info)
