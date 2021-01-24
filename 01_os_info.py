6f707742a4baf0844824c3365848db69c549584a
Igor Vlasov <vlasov.work@gmail.com>
2021-01-24 06:08:59 +0000

    Заработало!

--- 01_os_info.py
+++ 01_os_info.py
@@ -4,7 +4,6 @@ import sys
 
 info = 'OS info is \n{} \n\nPython version is {} {}'.format(platform.uname(), sys.version, platform.architecture())
 print(info)
-print('it is work!!!!')
 
 with open('os_info.txt', 'w') as ff:
     ff.write(info)
